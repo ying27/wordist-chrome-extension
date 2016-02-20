ea72e3639b72d3985ea56858a765439e197eb34f
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2016-02-20 16:02:34 +0100

    added floating box with word information

--- background.js
+++ background.js
@@ -1,23 +1,27 @@
 var actions = {
   'search-def': 'build.js'
-}
+};
 
 function performCommand (tab, command) {
   chrome.tabs.executeScript(tab.id, {
     file: actions[command]
-  })
+  });
 }
 
 chrome.commands.onCommand.addListener(function (command) {
+  console.log('command', command);
+  debugger
   chrome.tabs.query({
     active: true, // the active tab
     lastFocusedWindow: true // in the active window
   }, function (tabArray) {
-    var activeTab = tabArray[0]
+    var activeTab = tabArray[0];
     if (!activeTab) throw new Error('no active tab (?)')
-    performCommand(activeTab, command)
-  })
-})
+    performCommand(activeTab, command);
+  });
+});
+
+console.log('background is up');
 
 /*
 chrome.browserAction.onClicked.addListener(function (tab) {
--- search_word.js
+++ search_word.js
@@ -1,66 +1,67 @@
 var zepto = require('zepto-browserify');
 var $ = zepto.$;
 
-function getSelectionCoords(win) {
-    win = win || window;
-    var doc = win.document;
-    var sel = doc.selection, range, rects, rect;
-    var x = 0, y = 0;
-    if (sel) {
-        if (sel.type != "Control") {
-            range = sel.createRange();
-            range.collapse(true);
-            x = range.boundingLeft;
-            y = range.boundingTop;
-        }
-    } else if (win.getSelection) {
-        sel = win.getSelection();
-        if (sel.rangeCount) {
-            range = sel.getRangeAt(0).cloneRange();
-            if (range.getClientRects) {
-                range.collapse(true);
-                rects = range.getClientRects();
-                if (rects.length > 0) {
-                    rect = rects[0];
-                }
-                x = rect.left;
-                y = rect.top;
-            }
-            // Fall back to inserting a temporary element
-            if (x == 0 && y == 0) {
-                var span = doc.createElement("span");
-                if (span.getClientRects) {
-                    // Ensure span has dimensions and position by
-                    // adding a zero-width space character
-                    span.appendChild( doc.createTextNode("\u200b") );
-                    range.insertNode(span);
-                    rect = span.getClientRects()[0];
-                    x = rect.left;
-                    y = rect.top;
-                    var spanParent = span.parentNode;
-                    spanParent.removeChild(span);
-
-                    // Glue any broken text nodes back together
-                    spanParent.normalize();
-                }
-            }
-        }
+function getSelectionCoords (win) {
+  win = win || window;
+  var doc = win.document;
+  var sel = doc.selection, range, rects, rect;
+  var x = 0, y = 0;
+  if (sel) {
+    if (sel.type != 'Control') {
+      range = sel.createRange();
+      range.collapse(true);
+      x = range.boundingLeft;
+      y = range.boundingTop;
     }
-    return { x: x, y: y };
+  } else if (win.getSelection) {
+    sel = win.getSelection();
+    if (sel.rangeCount) {
+      range = sel.getRangeAt(0).cloneRange();
+      if (range.getClientRects) {
+        range.collapse(true);
+        rects = range.getClientRects();
+        if (rects.length > 0) {
+          rect = rects[0];
+        }
+        x = rect.left;
+        y = rect.top;
+      }
+      // Fall back to inserting a temporary element
+      if (x == 0 && y == 0) {
+        var span = doc.createElement('span');
+        if (span.getClientRects) {
+          // Ensure span has dimensions and position by
+          // adding a zero-width space character
+          span.appendChild(doc.createTextNode('\u200b'));
+          range.insertNode(span);
+          rect = span.getClientRects()[0];
+          x = rect.left;
+          y = rect.top;
+          var spanParent = span.parentNode;
+          spanParent.removeChild(span);
+
+          // Glue any broken text nodes back together
+          spanParent.normalize();
+        }
+      }
+    }
+  }
+  return { x: x, y: y };
 }
 
-var sel = window.getSelection()
-
+var sel = window.getSelection();
 
 if (sel.anchorNode != null) {
-    sel = sel.toString();
-    console.log('wololo')
+  sel = sel.toString();
 
-    var container = document.createElement('div')
-    container.innerHTML = '<p>hi</p>'
-    container.className = 'hackdediccionario'
-    document.body.appendChild(container)
+  var container = document.createElement('div');
+  container.innerHTML = "<p class='searchedWord'>" + sel +
+    "</p><hr/><p class='wordDefinition'>be or remain hidden so as to wait in ambush for someone or something.</p>";
+  container.className = 'hackdediccionario';
+  document.body.appendChild(container);
 
-
-    //console.log(getSelectionCoords());
+  setTimeout(function () {
+    $('.hackdediccionario').remove();
+  }, 8000);
+// console.log(getSelectionCoords());
 }
